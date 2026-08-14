324688f1766e8e42e1d807270f09b983aec960f3
Julian Rojas <juliandrojasa140201@gmail.com>
2026-08-14 09:55:53 -0500

    feat: crear endpoint de tareas

--- server/index.js
+++ server/index.js
@@ -1,26 +1,15 @@
-import cors from "cors";
-import express from 'express';
+import express from "express";
 
 const app = express();
 
-app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({
-    message: "API TO-DO funcionando",
-  })  
-})
 app.get("/api/todos", (req, res) => {
-  res.json({
-    message: "Lista de tareas",
-    todos: [],
-  });
+  res.json([]);
 });
 
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Servidor iniciado en: http://localhost${PORT}`);
-})
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
