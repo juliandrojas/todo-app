0120324b4ce51a6bf673f36feb50f6c182b1f28f
Julian Rojas <juliandrojasa140201@gmail.com>
2026-08-14 10:52:44 -0500

    feat:crear tarea

--- server/index.js
+++ server/index.js
@@ -4,11 +4,27 @@ const app = express();
 
 app.use(express.json());
 
+const todos = [];
+
 app.get("/api/todos", (req, res) => {
   res.json({
-  message: "Lista de tareas",
-  todos: [],
+    message: "Lista de tareas",
+    todos: todos,
+  });
 });
+
+app.post("/api/todos", (req, res) => {
+  const { title, completed } = req.body;
+
+  const todo = {
+    id: todos.length + 1,
+    title,
+    completed: completed ?? false,
+  };
+
+  todos.push(todo);
+
+  res.status(201).json(todo);
 });
 
 const PORT = process.env.PORT || 3000;
