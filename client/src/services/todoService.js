e1d6836a6d395e6d84e6f7e1c728a2ffe4d65a22
Julian Rojas <juliandrojasa140201@gmail.com>
2026-08-18 15:31:11 -0500

    feat: create todo service

---
+++ client/src/services/todoService.js
@@ -0,0 +1,17 @@
+import api from "../config/api";
+export const getTodos = async () => {
+    const response = await api.get("/todos");
+    return response.data;
+}
+export const getTodos = async () => {
+    const response = await api.post("/todos", todo);
+    return response.data;
+}
+export const updateTodo = async (id, todo) => {
+    const response = await api.put(`/todos/${id}`, todo);
+    return response.data;
+}
+export const deleteTodo = async (id, todo) => {
+    const response = await api.delete(`/todos/${id}`, todo);
+    return response.data;
+}
