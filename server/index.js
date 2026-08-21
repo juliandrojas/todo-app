8a602b1bde6a427aea05cf7cb8b102afbd13caf2
Julian Rojas <juliandrojasa140201@gmail.com>
2026-08-21 15:20:47 -0500

    fix: url-def

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(
     origin: [
       "http://localhost:5173",
       "http://127.0.0.1:5173",
-      "https://todo-app-client-zeta.vercel.app",
+      "https://todoapp-production.vercel.app",
     ],
     credentials: true,
   })
